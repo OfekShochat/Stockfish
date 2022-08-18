07caabb3d6150d2d5c5c8d46356d00b600d70785
ghostway <50481015+OfekShochat@users.noreply.github.com>
2022-08-18 15:04:08 +0300

    bench 6114555

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
 
     else if (PvNode && depth <= 6 && eval < beta && eval > complexity * alpha / 2)
     {
-        eval = qsearch<PV>(pos, ss, (Value)complexity / depth + eval, beta); // Narrow the window, with some margin (more leafy = more margin)
+        eval = qsearch<PV>(pos, ss, alpha + eval * depth / Value(4), beta); // Narrow the window, with some margin (more leafy = more margin)
         if (eval >= beta)
             return eval;
     }
